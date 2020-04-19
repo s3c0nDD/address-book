b5f6cccabb8bce06bba79750dcf81ea0110f2b56
s3c0nDD <s3c0nDD@users.noreply.github.com>
2020-04-19 19:47:43 +0200

    Updates

---
+++ precache-manifest.c3a48a7324c08ccd52b36df74b13c027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8050f9f131c55b59b965d10322fbbaf",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "4a8d638665248aea00e5",
+    "url": "/address-book/static/js/2.e5647af1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/address-book/static/js/2.e5647af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27689e1590b25fb7f25",
+    "url": "/address-book/static/js/main.c87b3568.chunk.js"
+  },
+  {
+    "revision": "dd10132569fcf52296ba",
+    "url": "/address-book/static/js/runtime-main.992209da.js"
+  }
+]);

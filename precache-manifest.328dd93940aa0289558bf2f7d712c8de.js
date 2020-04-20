a3317da05346a0e8c67c3082aa5ad6f78e064c14
s3c0nDD <s3c0nDD@users.noreply.github.com>
2020-04-21 01:28:43 +0200

    Updates

---
+++ precache-manifest.328dd93940aa0289558bf2f7d712c8de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0a33788d7dd759d73fe749927906ec",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "fefb5629413154087633",
+    "url": "/address-book/static/js/2.87ca1a16.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/address-book/static/js/2.87ca1a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24947a2a8ae508e39c0",
+    "url": "/address-book/static/js/main.0c0edfe1.chunk.js"
+  },
+  {
+    "revision": "dd10132569fcf52296ba",
+    "url": "/address-book/static/js/runtime-main.992209da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/address-book/precache-manifest.c155524d1add3d7925ce20bc1522272f.js"
+  "/address-book/precache-manifest.328dd93940aa0289558bf2f7d712c8de.js"
 );
 
 self.addEventListener('message', (event) => {

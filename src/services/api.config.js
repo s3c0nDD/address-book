ccdcb4de0e800f16bd81b394a1ed8f4911b91a20
s3c0nDD <s3c0nDD@users.noreply.github.com>
2020-04-21 23:42:07 +0200

    Add seed for usage in api calls to make results consistent

--- src/services/api.config.js
+++ src/services/api.config.js
@@ -1,5 +1,17 @@
+/**
+ * The API URL address.
+ * @constant
+ *
+ * @type {string}
+ */
 export const API_URL = 'https://randomuser.me/api/';
 
+/**
+ * Default keys, which should be taken from api result's user entity.
+ * @constant
+ *
+ * @type {array<string>}
+ */
 export const API_PICK_ONLY_KEYS = [
   'name',
   'location',
@@ -12,11 +24,31 @@ export const API_PICK_ONLY_KEYS = [
   'nat'
 ];
 
+/**
+ * Default count of users to fetch.
+ * @constant
+ *
+ * @type {number}
+ */
 export const DEFAULT_USERS_COUNT = 1;
 
+/**
+ * Language code mappings.
+ * @constant
+ *
+ * @type {object}
+ */
 export const LANGUAGE_CODES = {
   british: 'GB',
   french: 'FR',
   spanish: 'ES',
   swiss: 'CH'
 };
+
+/**
+ * Default seed string used to make api results consistent
+ * @constant
+ *
+ * @type {string}
+ */
+export const SEED_STRING = 's3c0nDD';
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -4,12 +4,25 @@ import {
   API_URL,
   API_PICK_ONLY_KEYS,
   DEFAULT_USERS_COUNT,
-  LANGUAGE_CODES
+  LANGUAGE_CODES,
+  SEED_STRING
 } from './api.config';
 
+/**
+ * Call API endpoint to get users.
+ * @async
+ *
+ * @param {array} [nationalities] list of nationalities of fetched users
+ * @param {number} [usersCount=CONFIG.DEFAULT_USERS_COUNT] users count to fetch
+ * @param {string} [seed=CONFIG.SEED_STRING]
+ * @param {array<string>} [takeKeys=CONFIG.API_PICK_ONLY_KEYS] list of keys to take from user entity
+ *
+ * @returns {Promise<Array>} promise of fetched users
+ */
 export const getUsers = ({
   nationalities = [],
   usersCount = DEFAULT_USERS_COUNT,
+  seed = SEED_STRING,
   takeKeys = API_PICK_ONLY_KEYS
 } = {}) => {
   const nationalitiesCodes = nationalities.map((name) => {
@@ -24,7 +37,8 @@ export const getUsers = ({
     params: {
       ...(takeKeys && { inc: takeKeys.join(',') }),
       ...(usersCount && { results: usersCount }),
-      ...(nationsQueryParam && { nat: nationsQueryParam })
+      ...(nationsQueryParam && { nat: nationsQueryParam }),
+      ...(seed && { seed })
     }
   })
     .then(response => {

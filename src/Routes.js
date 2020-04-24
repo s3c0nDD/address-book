204632825f64b5a24b9058dc8f086aa65fc0a8c9
s3c0nDD <s3c0nDD@users.noreply.github.com>
2020-04-24 13:46:28 +0200

    Add code splitting

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import MainPage from './pages/MainPage';
-import SettingsPage from './pages/SettingsPage';
-import NotFoundPage from './pages/NotFoundPage';
+const MainPage = lazy(() => import('./pages/MainPage'));
+const SettingsPage = lazy(() => import('./pages/SettingsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 export const ROUTES = {
   root: '/',
@@ -12,20 +12,22 @@ export const ROUTES = {
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route
-        path={ROUTES.root}
-        component={MainPage}
-        exact
-      />
-      <Route
-        path={ROUTES.settings}
-        component={SettingsPage}
-      />
-      <Route
-        component={NotFoundPage}
-      />
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route
+          path={ROUTES.root}
+          component={MainPage}
+          exact
+        />
+        <Route
+          path={ROUTES.settings}
+          component={SettingsPage}
+        />
+        <Route
+          component={NotFoundPage}
+        />
+      </Switch>
+    </Suspense>
   );
 };
 
